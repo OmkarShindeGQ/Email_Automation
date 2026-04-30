529307f98f69566513f562353007d04a008667b8
OmkarShindeGQ <omkar.shinde@grayquest.com>
2026-04-30 13:59:03 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -9,7 +9,7 @@ def send_email():
     msg["To"] = "rajat.singh@grayquest.com"
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-        server.login("omkar.shinde@grayquest.com", os.environ["EMAIL_PASS"])
+        server.login("omkar.shinde@grayquest.com", os.environ["tdao xbnx cqwd nsbb"])
         server.send_message(msg)
 
 send_email()
