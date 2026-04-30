7755e16104816be83900062ed7d7d41ef2e566ad
OmkarShindeGQ <omkar.shinde@grayquest.com>
2026-04-30 13:46:31 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -3,13 +3,13 @@ from email.mime.text import MIMEText
 import os
 
 def send_email():
-    msg = MIMEText("Hello! This is your daily automated email.")
+    msg = MIMEText("Hello Rajat, this is your daily automated email.")
     msg["Subject"] = "Daily Update"
-    msg["From"] = os.environ["EMAIL_USER"]
-    msg["To"] = os.environ["EMAIL_TO"]
+    msg["From"] = "omkar.shinde@grayquest.com"
+    msg["To"] = "rajat.singh@grayquest.com"
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-        server.login(os.environ["EMAIL_USER"], os.environ["EMAIL_PASS"])
+        server.login("omkar.shinde@grayquest.com", os.environ["EMAIL_PASS"])
         server.send_message(msg)
 
 send_email()
